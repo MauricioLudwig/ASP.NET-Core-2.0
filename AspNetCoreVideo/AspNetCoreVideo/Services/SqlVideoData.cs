2de50c98dc4c9887e4e32665874a9691dba4942f
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2018-04-20 23:45:49 +0200

    Ch. 6

---
+++ AspNetCoreVideo/AspNetCoreVideo/Services/SqlVideoData.cs
@@ -0,0 +1,37 @@
+﻿using AspNetCoreVideo.Data;
+using AspNetCoreVideo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreVideo.Services
+{
+
+    public class SqlVideoData : IVideoData
+    {
+
+        private VideoDbContext _db;
+
+        public SqlVideoData(VideoDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Add(Video video)
+        {
+            _db.Add(video);
+            _db.SaveChanges();
+        }
+
+        public Video Get(int id)
+        {
+            return _db.Find<Video>(id);
+        }
+
+        public IEnumerable<Video> GetAll()
+        {
+            return _db.Videos;
+        }
+    }
+}
--- AspNetCoreVideo/AspNetCoreVideo/Startup.cs
+++ AspNetCoreVideo/AspNetCoreVideo/Startup.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreVideo.Data;
 using AspNetCoreVideo.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -17,11 +19,18 @@ namespace AspNetCoreVideo
 
         public IConfiguration Configuration { get; set; }
 
-        public Startup()
+        public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json");
+                //.AddJsonFile("appsettings.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
             Configuration = builder.Build();
         }
 
@@ -29,10 +38,14 @@ namespace AspNetCoreVideo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var conn = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<VideoDbContext>(options =>
+                options.UseSqlServer(conn));
+
             services.AddMvc();
             services.AddSingleton(provider => Configuration);
             services.AddSingleton<IMessageService, ConfigurationMessageService>();
-            services.AddSingleton<IVideoData, MockVideoData>();
+            services.AddScoped<IVideoData, SqlVideoData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
