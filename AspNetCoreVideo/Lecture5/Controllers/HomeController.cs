c42e2e4076f411601fece3112f85a8c4c6b7dde0
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2018-05-15 21:18:27 +0200

    Lecture5

---
+++ AspNetCoreVideo/Lecture5/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lecture5.Models;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Targets;
+
+namespace Lecture5.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private ILogger<HomeController> logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            logger.LogInformation("Index page says hello");
+
+            DateTime shortDate = new DateTime(2018, 5, 15);
+            var file = (FileTarget)LogManager.Configuration.FindTargetByName("ownFile-web");
+            var logEventInfo = new LogEventInfo { TimeStamp = shortDate };
+            string fileName = file.FileName.Render(logEventInfo);
+
+            return View(System.IO.File.ReadAllLines(fileName));
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AspNetCoreVideo/Lecture5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace Lecture5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                BuildWebHost(args).Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging => {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog()
+                .Build();
+    }
+}
